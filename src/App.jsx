172cefc6281e3ef689a7425c5610489de95247fd
Bikashxkhanal <khanalbikash007@gmail.com>
2025-10-13 21:38:36 +0545

    added logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,67 @@
-
-import './App.css'
-import {CurrencyContainer} from './components/currencybox'
+import { useState } from "react";
+import "./App.css";
+import { CurrencyContainer } from "./components/currencybox";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
-  
-  
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("npr");
+  const [convertAmt, setConvertAmt] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
+  const currencyoptions = Object.keys(currencyInfo);
+
+  const convert = () => {
+    setConvertAmt(amount * currencyInfo[to]);
+  };
+
+  function swap() {
+    setFrom(to);
+    setTo(from);
+    setAmount(convertAmt);
+    setConvertAmt(amount);
+  }
+
   return (
     <>
-     <div className=' fixed inset-0 bg-[url(./assets/image.jpg)] bg-cover bg-no-repeat flex flex-col justify-center items-center bg-center shadow-xl'>
-      <div className=' w-sm m-w-sm py-2 rounded-lg bg-white/30 shadow-md  '>
-        <div className='relative flex flex-col gap-1'>
-        < CurrencyContainer status = "From" />
-        <button className='absolute bg-blue-600 text-white px-2 rounded-sm top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 '>Swap</button>
-        < CurrencyContainer status= "To" />
-        </div >
-        <button className='px-3 py-2 mt-5 mb-2 text-white border-2 bg-green-600  rounded-xl'>Convert USD to INR</button>
-         </div>
-     </div>
+      <div className=" fixed inset-0 bg-[url(./assets/image.jpg)] bg-cover bg-no-repeat flex flex-col justify-center items-center bg-center shadow-xl">
+        <div className=" w-sm m-w-sm py-2 rounded-lg bg-white/30 shadow-md  ">
+          <div className="relative flex flex-col gap-1">
+            <CurrencyContainer
+              label="From"
+              amount={amount}
+              currencyOptions={currencyoptions}
+              onCurrencyChange={(currency) => setAmount(amount)}
+              selectedCurrency={from}
+              onAmtChange={(amount)=>setAmount(amount)}
+            />
+            <button
+              className="absolute bg-blue-600 text-white px-2 rounded-sm top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 "
+              onClick={swap}
+            >
+              Swap
+            </button>
+            <CurrencyContainer
+              label="To"
+              amount={convertAmt}
+              currencyOptions={currencyoptions}
+              onCurrencyChange={() => setConvertAmt(convertAmt)}
+              selectedCurrency={to}
+              amountDisabled
+              
+            />
+          </div>
+          <button
+            className="px-3 py-2 mt-5 mb-2 text-white border-2 bg-green-600  rounded-xl"
+            onClick={convert}
+          >
+            Convert {from.toUpperCase()} to {to.toUpperCase()}
+          </button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/currencybox.jsx
+++ src/components/currencybox.jsx
@@ -1,22 +1,41 @@
 import React from "react";
 import '../../src/index.css'
+import { useId } from "react";
 
-export function CurrencyContainer({status}){
- console.log(status);
- 
- 
-    return(
+//container with the ip field , curreyncy type etc(reusable component)
+export function CurrencyContainer({ label, 
+    amount,
+    onAmtChange,
+    currencyOptions = [],
+    onCurrencyChange,
+    selectedCurrency = "USD",
+    amountDisabled = false,
+})
+    {
+  const ipId = useId();
+   
+     
+
+    return( 
         <>
         <div className="bg-white mt-2  mx-2 text-sm flex-col rounded-xl py-2 ">
            <div className="flex justify-between w-full px-5 text-black/50 "> 
-            <label htmlFor="">{status}</label>
+            <label htmlFor={ipId}>{label}</label>
             <label htmlFor="">Currency Type</label>
            </div>
            <div className="flex justify-between w-full px-5">
-            <input type="number" className="outline-none  h-7 " /> 
+            <input type="number" id={ipId} value={amount} disabled = {amountDisabled} 
+            onChange={(e)=> onAmtChange && onAmtChange(Number(e.target.value))}
+            className="outline-none  h-7 " /> 
            
-                <select name="" id="" className="bg-gray-300 rounded-lg px-1" >
-                    <option value="">USD</option>
+                <select name="" id="" value={selectedCurrency} 
+                onChange={(e)=> onCurrencyChange && onCurrencyChange(e.target.value) }    
+                className= "outline-none bg-gray-300 rounded-lg px-1" >
+                    {currencyOptions.map((currency)=>(
+                        <option key={currency}
+                         value={currency}>{currency}</option>
+                    ))} 
+   
                 </select>
             
            </div>
